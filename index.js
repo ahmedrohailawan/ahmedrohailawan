35cd167e7ac16bfe3833ea8ec0a63711b29adef5
Ahmed Rohail Awan <ahmedrohailawanwaheedyousaf@gmail.com>
2022-09-05 01:32:05 +0500

    updated

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing",{
+      strings: ["Freelancer","Developer","Designer","Blogger","YouTuber"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop:true
+    });
+});
