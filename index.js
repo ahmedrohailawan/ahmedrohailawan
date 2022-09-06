9411761197a3f22349aa4a50daccbed63e88f6a2
Ahmed Rohail Awan <ahmedrohailawanwaheedyousaf@gmail.com>
2022-09-06 21:36:00 +0500

    updated

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
       backSpeed: 60,
       loop:true
     });
+    $('.carousel').owlCarousel({
+      margin :20, 
+      loop:true,
+      autoplayTime:2000,
+      autoplayHoverPause:true,
+      responsive: {
+          0:{
+              items :1,
+              nav:false
+          },
+          600:{
+              items :2,
+              nav:false
+          },
+          1000:{
+              items :3,
+              nav:false
+          }
+      }
+  });
 });
